c6a68e62840608e9451881ac267becbd088fee32
Antti Rask <antti.rask@teosto.fi>
2023-12-06 20:40:17 +0200

    Trying to get the OPENAI_API_KEY to work.

--- fetch_image.R
+++ fetch_image.R
@@ -45,8 +45,10 @@ body    <- list(
 
 # For the request you need to replace the OPENAI_API_KEY with your own API key
 # that you get after signing up: https://platform.openai.com/account/api-keys
+OPENAI_API_KEY <- Sys.getenv(OPENAI_API_KEY)
+
 request <- request(url) %>%
-    req_headers(Authorization = str_glue("Bearer {Sys.getenv(OPENAI_API_KEY)}")) %>%
+    req_headers(Authorization = str_glue("Bearer {OPENAI_API_KEY}")) %>%
     req_body_json(body) %>%
     req_perform()
 

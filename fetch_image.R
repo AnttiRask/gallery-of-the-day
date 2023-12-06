3c3fdf445421006b1eefcc415f22ec2d2866b244
Antti Rask <antti.rask@teosto.fi>
2023-12-06 23:46:05 +0200

    httr2 -> httr

--- fetch_image.R
+++ fetch_image.R
@@ -6,7 +6,8 @@ library(conflicted)
     conflicts_prefer(dplyr::filter)
 library(curl)
 library(dplyr)
-library(httr2)
+library(httr)
+# library(httr2)
 library(jsonlite)
 library(lubridate)
 library(purrr)
@@ -53,10 +54,18 @@ body    <- list(
 # that you get after signing up: https://platform.openai.com/account/api-keys
 OPENAI_API_KEY <- Sys.getenv("OPENAI_API_KEY")
 
-request <- request(url) %>%
-    req_headers(Authorization = str_glue("Bearer {OPENAI_API_KEY}")) %>%
-    req_body_json(body) %>%
-    req_perform()
+# request <- request(url) %>%
+#     req_headers(Authorization = str_glue("Bearer {OPENAI_API_KEY}")) %>%
+#     req_body_json(body) %>%
+#     req_perform()
+
+response <- POST(
+    url,
+    add_headers(Authorization = str_glue("Bearer {OPENAI_API_KEY}")),
+    body   = body,
+    encode = "json",
+    timeout(300)
+)
 
 # Check the request was successful (status code should be 200) ----
 request$status_code
@@ -77,9 +86,6 @@ url_img <- request %>%
 
 # Create the destination file name
 destfile <- str_glue("img/gallery-of-the-day-{today()}.png")
-
-# Set a longer timeout globally (e.g., 120 seconds)
-options(timeout = 300)
 
 # Download the file at the URLand save it to 'destfile'
 curl_download(url_img, destfile, mode = "wb")

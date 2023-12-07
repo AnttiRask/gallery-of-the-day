0d0a676dc683bf341b64acce46835ea80cb163fb
Antti Rask <antti.rask@teosto.fi>
2023-12-07 20:43:44 +0200

    Updated deploy_app.R to have a local version of the deployment procedure.

--- deploy_app.R
+++ deploy_app.R
@@ -1,9 +1,19 @@
 library(rsconnect)
 
+# GitHub Actions
 setAccountInfo(
     name   = Sys.getenv("SHINY_APPS_NAME"),
     token  = Sys.getenv("SHINY_APPS_TOKEN"),
     secret = Sys.getenv("SHINY_APPS_SECRET")
 )
+
+# Locally
+# source("secret.R")
+# 
+# setAccountInfo(
+#     name   = SHINY_APPS_NAME,
+#     token  = SHINY_APPS_TOKEN,
+#     secret = SHINY_APPS_SECRET
+# )
 
 deployApp()

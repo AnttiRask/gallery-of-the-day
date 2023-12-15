053e2fd6242ddd53a5a2e61d20f2c9efeefa1c29
Antti Rask <antti.rask@teosto.fi>
2023-12-15 23:34:30 +0200

    Getting ready for GitHub Actions

--- deploy_app.R
+++ deploy_app.R
@@ -1,20 +1,20 @@
 library(rsconnect)
 
 # GitHub Actions
-# setAccountInfo(
-#     name   = Sys.getenv("SHINY_APPS_NAME"),
-#     token  = Sys.getenv("SHINY_APPS_TOKEN"),
-#     secret = Sys.getenv("SHINY_APPS_SECRET")
-# )
+setAccountInfo(
+    name   = Sys.getenv("SHINY_APPS_NAME"),
+    token  = Sys.getenv("SHINY_APPS_TOKEN"),
+    secret = Sys.getenv("SHINY_APPS_SECRET")
+)
 
 # Locally
-source("secret.R")
+# source("secret.R")
 
-setAccountInfo(
-    name   = SHINY_APPS_NAME,
-    token  = SHINY_APPS_TOKEN,
-    secret = SHINY_APPS_SECRET
-)
+# setAccountInfo(
+#     name   = SHINY_APPS_NAME,
+#     token  = SHINY_APPS_TOKEN,
+#     secret = SHINY_APPS_SECRET
+# )
 
 deployApp(
     appDir      = "app/",
